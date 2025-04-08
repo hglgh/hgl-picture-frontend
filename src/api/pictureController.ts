998f40f97fe6975585934c83f3295684524f02e3
HGL <13479874797@163.com>
2025-04-08 13:15:08 +0800

    feat(api): 添加文件、图片和空间相关的 API 接口
    - 新增 fileController.ts、pictureController.ts 和 spaceController.ts 文件
    - 实现了文件下载、上传接口
    - 实现了图片相关的一系列接口，包括删除、编辑、获取、列表、上传等
    - 实现了空间相关的一系列接口，包括添加、删除、编辑、获取、列表等

---
+++ src/api/pictureController.ts
@@ -0,0 +1,217 @@
+// @ts-ignore
+/* eslint-disable */
+import request from '@/request'
+
+/** deletePicture POST /hgl/picture/delete */
+export async function deletePictureUsingPost(
+  body: API.DeleteRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseBoolean_>('/hgl/picture/delete', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** editPicture POST /hgl/picture/edit */
+export async function editPictureUsingPost(
+  body: API.PictureEditRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseBoolean_>('/hgl/picture/edit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** getPictureById GET /hgl/picture/get */
+export async function getPictureByIdUsingGet(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getPictureByIdUsingGETParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePicture_>('/hgl/picture/get', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  })
+}
+
+/** getPictureVOById GET /hgl/picture/get/vo */
+export async function getPictureVoByIdUsingGet(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getPictureVOByIdUsingGETParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePictureVO_>('/hgl/picture/get/vo', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  })
+}
+
+/** listPictureByPage POST /hgl/picture/list/page */
+export async function listPictureByPageUsingPost(
+  body: API.PictureQueryRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePagePicture_>('/hgl/picture/list/page', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** listPictureVOByPage POST /hgl/picture/list/page/vo */
+export async function listPictureVoByPageUsingPost(
+  body: API.PictureQueryRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePagePictureVO_>('/hgl/picture/list/page/vo', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** listPictureVOByPageWithCache POST /hgl/picture/list/page/vo/cache */
+export async function listPictureVoByPageWithCacheUsingPost(
+  body: API.PictureQueryRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePagePictureVO_>('/hgl/picture/list/page/vo/cache', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** doPictureReview POST /hgl/picture/review */
+export async function doPictureReviewUsingPost(
+  body: API.PictureReviewRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseBoolean_>('/hgl/picture/review', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** listPictureTagCategory GET /hgl/picture/tag_category */
+export async function listPictureTagCategoryUsingGet(options?: { [key: string]: any }) {
+  return request<API.BaseResponsePictureTagCategory_>('/hgl/picture/tag_category', {
+    method: 'GET',
+    ...(options || {}),
+  })
+}
+
+/** updatePicture POST /hgl/picture/update */
+export async function updatePictureUsingPost(
+  body: API.PictureUpdateRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseBoolean_>('/hgl/picture/update', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** uploadPicture POST /hgl/picture/upload */
+export async function uploadPictureUsingPost(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.uploadPictureUsingPOSTParams,
+  body: {},
+  file?: File,
+  options?: { [key: string]: any }
+) {
+  const formData = new FormData()
+
+  if (file) {
+    formData.append('file', file)
+  }
+
+  Object.keys(body).forEach((ele) => {
+    const item = (body as any)[ele]
+
+    if (item !== undefined && item !== null) {
+      if (typeof item === 'object' && !(item instanceof File)) {
+        if (item instanceof Array) {
+          item.forEach((f) => formData.append(ele, f || ''))
+        } else {
+          formData.append(ele, JSON.stringify(item))
+        }
+      } else {
+        formData.append(ele, item)
+      }
+    }
+  })
+
+  return request<API.BaseResponsePictureVO_>('/hgl/picture/upload', {
+    method: 'POST',
+    params: {
+      ...params,
+    },
+    data: formData,
+    requestType: 'form',
+    ...(options || {}),
+  })
+}
+
+/** uploadPictureByBatch POST /hgl/picture/upload/batch */
+export async function uploadPictureByBatchUsingPost(
+  body: API.PictureUploadByBatchRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseInt_>('/hgl/picture/upload/batch', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** uploadPictureByUrl POST /hgl/picture/upload/url */
+export async function uploadPictureByUrlUsingPost(
+  body: API.PictureUploadRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePictureVO_>('/hgl/picture/upload/url', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
---
+++ src/api/spaceController.ts
@@ -0,0 +1,131 @@
+// @ts-ignore
+/* eslint-disable */
+import request from '@/request'
+
+/** addSpace POST /hgl/space/add */
+export async function addSpaceUsingPost(
+  body: API.SpaceAddRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseLong_>('/hgl/space/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** deleteSpace POST /hgl/space/delete */
+export async function deleteSpaceUsingPost(
+  body: API.DeleteRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseBoolean_>('/hgl/space/delete', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** editSpace POST /hgl/space/edit */
+export async function editSpaceUsingPost(
+  body: API.SpaceEditRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseBoolean_>('/hgl/space/edit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** getSpaceById GET /hgl/space/get */
+export async function getSpaceByIdUsingGet(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getSpaceByIdUsingGETParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseSpace_>('/hgl/space/get', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  })
+}
+
+/** getSpaceVOById GET /hgl/space/get/vo */
+export async function getSpaceVoByIdUsingGet(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getSpaceVOByIdUsingGETParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseSpaceVO_>('/hgl/space/get/vo', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  })
+}
+
+/** listSpaceLevel GET /hgl/space/list/level */
+export async function listSpaceLevelUsingGet(options?: { [key: string]: any }) {
+  return request<API.BaseResponseListSpaceLevel_>('/hgl/space/list/level', {
+    method: 'GET',
+    ...(options || {}),
+  })
+}
+
+/** listSpaceByPage POST /hgl/space/list/page */
+export async function listSpaceByPageUsingPost(
+  body: API.SpaceQueryRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePageSpace_>('/hgl/space/list/page', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** listSpaceVOByPage POST /hgl/space/list/page/vo */
+export async function listSpaceVoByPageUsingPost(
+  body: API.SpaceQueryRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePageSpaceVO_>('/hgl/space/list/page/vo', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** updateSpace POST /hgl/space/update */
+export async function updateSpaceUsingPost(
+  body: API.SpaceUpdateRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseBoolean_>('/hgl/space/update', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
